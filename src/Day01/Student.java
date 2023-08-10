e38267682ad9fa038ad7ff8a44a6705e6fcad814
Deniz <dnz66@hotmail.com>
2023-08-09 20:54:25 -0400

    our fifth commit

--- src/Day01/Student.java
+++ src/Day01/Student.java
@@ -1,5 +1,13 @@
 package Day01;
 
 public class Student {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        for (int i = 0; i <10; i++) {
+            System.out.println(i);
+        }
+    }
 
 }
